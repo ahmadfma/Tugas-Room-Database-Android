8a07c80d7608307e1f011e4f814ab36904cf1b75
ahmadfma <ahmadfathanah5@gmail.com>
2021-05-21 16:17:46 +0800

    set MainActivity recyclerview

--- app/src/main/java/com/example/roomdb/MainActivity.kt
+++ app/src/main/java/com/example/roomdb/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.roomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.roomdb.User.UserViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: UserViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        recyclerview.setHasFixedSize(true)
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        viewModel.getNotes()?.observe(this, Observer {
+            if(it.isNotEmpty()) {
+                recyclerview.adapter = MainAdapter(it)
+            }
+        })
+
     }
 }
