aa7f129e647961ae2de9d39a93b49b3fc4f82ab0
ahmadfma <ahmadfathanah5@gmail.com>
2021-05-21 17:11:08 +0800

    Add action to Add Button

--- app/src/main/java/com/example/roomdb/MainActivity.kt
+++ app/src/main/java/com/example/roomdb/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.roomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.roomdb.User.UserEntity
 import com.example.roomdb.User.UserViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_add.*
+import kotlinx.android.synthetic.main.dialog_add.view.*
+import kotlinx.android.synthetic.main.item_note.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,5 +31,29 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        addbtn.setOnClickListener {
+            addbtnAction()
+        }
+
     }
+
+    private fun addbtnAction() {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_add, null)
+        val builder = AlertDialog.Builder(this)
+            .setView(dialogView)
+        val mDialog = builder.show()
+        dialogView.simpan_btn.setOnClickListener {
+            val judul = ""+dialogView.judul.text
+            val note = ""+dialogView.note.text
+            if(judul.trim().isNotEmpty() && note.trim().isNotEmpty()) {
+                mDialog.dismiss()
+                viewModel.insertNote(UserEntity(0,""+judul, ""+note))
+            }
+        }
+        dialogView.cancel_button.setOnClickListener {
+            mDialog.dismiss()
+        }
+
+    }
+
 }
